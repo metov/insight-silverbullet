c81141aa0970ecfa3c8c92f1907a7d56ab832d0d
Azat Akhmetov <azatinfo@yandex.com>
2019-06-14 10:04:54 -0700

    File was accidentally committed.

--- kafka-producer/createTopic.sh
+++
@@ -1,6 +0,0 @@
-#######################################################
-# This Is The Script To Create A Topic named meta In Kafka
-#######################################################
-
-
-/usr/local/kafka/bin/kafka-topics.sh --create --zookeeper localhost:2181 --replication-factor 3 --partitions 2 --topic met
